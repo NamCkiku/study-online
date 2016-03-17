2ce155a82f80830b7bcfbf1fe760ae344e17a581
NamCkiku <tranhoangnam11373@gmail.com>
2016-03-17 12:47:04 +0700

    Add giao dien them khoa hoc

--- StudyOnline/StudyOnline.Repository/UserRepository.cs
+++ StudyOnline/StudyOnline.Repository/UserRepository.cs
@@ -8,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace StudyOnline.Repository
 {
-    public class UserRepository
+    public interface IUserRepository
+    {
+        List<StudyOnline.Entities.Models.User> ListAllUser();
+        StudyOnline.Entities.Models.User ViewDetail(long id);
+        long CreateUser(StudyOnline.Entities.Models.User tq);
+        bool UpdateUser(StudyOnline.Entities.Models.User us);
+        bool DeleteUser(long id);
+        int Login(string userName, string password);
+        bool Signup(User user);
+        void Active(int id, bool status);
+    }
+    public class UserRepository : IUserRepository
     {
         /// <summary>
         /// Danh sách người dùng
