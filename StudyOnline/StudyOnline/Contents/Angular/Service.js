1de303759d0615d27014485d40a41c25994c3531
NamCkiku <tranhoangnam11373@gmail.com>
2016-04-03 21:15:07 +0700

    add forder App-script

--- StudyOnline/StudyOnline/Contents/Angular/Service.js
+++
@@ -1,9 +0,0 @@
-﻿app.service("myService", ['$http', function ($http) {
-    var myService = {};
-    
-    myService.getDocumentCome = function () {
-        return $http.get("http://localhost:22508/study-online/user/");
-    };
-
-    return myService;
-}]);
