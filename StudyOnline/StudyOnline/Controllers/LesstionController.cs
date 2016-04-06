ecd63e3605d7b56c097ce9ac5f08eb8fbef1bfeb
NamCkiku <tranhoangnam11373@gmail.com>
2016-04-06 20:46:49 +0700

    add updateSecction

---
+++ StudyOnline/StudyOnline/Controllers/LesstionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyOnline.Controllers
+{
+    public class LesstionController : Controller
+    {
+        // GET: Lesstion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult _UpdateLesstion()
+        {
+            return PartialView();
+        }
+    }
+}
